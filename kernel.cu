36ca083be83690150790cbaadea3fd9a0ccd182a
OWNER <OWNER@DESKTOP-K96F2P7>
2023-07-12 22:58:39 -0500

    Added ability to pass a function to set the matrix. Also added matrix addition, scalar multipliacation, and matrix multiplicatoin.

--- kernel.cu
+++ kernel.cu
@@ -2,18 +2,48 @@
 
 int main()
 {
-    int** standardArr = (int**)malloc(3 * sizeof(int*));
-    for (int i = 0; i < 3; i++) {
-        standardArr[i] = (int*)malloc(4 * sizeof(int));
-        standardArr[i][0] = (i * 3) + 1;
-        standardArr[i][1] = (i * 3) + 2;
-        standardArr[i][2] = (i * 3) + 3;
-        standardArr[i][3] = (i * 3) + 4;
-    }
 
-    CMatrix<int> CMatrixObj = CMatrix<int>(3, 4);
-    CMatrixObj.setCMatrix(standardArr);
+    std::cout << "Mat1\n";
+
+    CMatrix CMatrixObj = CMatrix(5, 5);
+    std::function<double(int, int)> foo1;
+    foo1 = [](int x, int y) {
+        return (double)((y * y) + x);
+    };
+    CMatrixObj.setCMatrix(foo1);
     CMatrixObj.printCMatrix();
+
+    std::cout << "\nMat2\n";
+
+    CMatrix CMatrixObj1 = CMatrix(5, 5);
+    std::function<double(int, int)> foo; 
+    foo = [](int x, int y) {
+        return (double)((x * x) + y);
+    };
+    CMatrixObj1.setCMatrix(foo);
+    CMatrixObj1.printCMatrix();
+
+    std::cout << "\nMat3\n";
+    CMatrix CMatrixObj2 = CMatrix::add(CMatrixObj1, CMatrixObj);
+    CMatrixObj2.printCMatrix();
+
+    std::cout << "\nMat4\n";
+
+    std::function<double(int, int)> foo2;
+    foo2 = [](int x, int y) {
+        return (double)(x + 1);
+    };
+    CMatrix m1 = CMatrix(2, 2);
+    CMatrix m2 = CMatrix(2, 2);
+    m1.setCMatrix(foo2);
+    m2.setCMatrix(foo2);
+
+    CMatrix m3 = CMatrix::multiply(CMatrixObj1, CMatrixObj2);
+    m3.printCMatrix();
+
+
+
+
 
     return 0;
 }
